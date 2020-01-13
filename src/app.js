2d1a15ced342b8df40d4b9eb7ff03e89980964ee
Evan Smith <smith.m.evan@gmail.com>
2020-01-13 17:36:36 +0000

    todos can be added

--- src/app.js
+++ src/app.js
@@ -9,10 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       todos: [
         "Buy shopping", "Clean bathroom", "Car's MOT"
-        ]
+      ],
+
+      newToDo: ""
+    },
+
+    methods: {
+      saveNewToDo: function () {
+        this.todos.push(this.newToDo)
+      }
     }
 
-
   })
+
+
 
 });
